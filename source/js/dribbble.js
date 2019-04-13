ceca3710eff3d29e84274da2297d59adc73944c0
Jaroca124 <jacob.caccamo@gmail.com>
2019-04-13 14:46:25 -0400

    API Additions

--- source/js/dribbble.js
+++ source/js/dribbble.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
+    $('.tabs').tabs();
+
     // Set the Access Token
     if ($("#shots").length) {
         var accessToken = '6b1ab673c477ebcc8b2812c1e9bef03b521df396cb746ccd546002982f30c0e9';
@@ -12,7 +14,7 @@ $( document ).ready(function() {
             if (data.length > 0) { 
                 $.each(data.reverse(), function(i, val) {                
                 $('#shots').prepend(
-                    '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
+                    '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><h2 class="title">' + val.title + '</h2><img src="'+ val.images.hidpi +'"/></a>'
                     )
                 })
             }
@@ -21,5 +23,34 @@ $( document ).ready(function() {
             }
             }
         });
+
+        // INSTAGRAM
+        var igAccessToken = '8923463321.345b6eb.5ce918c2ce294c06baaab6161cb477cc';
+        var reqURL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token='+igAccessToken;
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+                response = JSON.parse(xhttp.responseText);
+                loadInstagramPosts(response.data);
+            }
+        };
+        xhttp.open("GET", reqURL, true);
+        xhttp.send();
     }
 });
+
+function loadInstagramPosts(data) {
+    if (data.length > 0) {
+        for (i = 0; i < data.length; i++) { 
+            let val = data[i];
+            $('#instagram_posts').append(
+                '<a class="post" target="_blank" href="'+ val.link +'"><img src="'+ val.images.standard_resolution.url +'" alt="' + val.caption + '"/><p class="caption">' + val.caption.text + '</p></a>'
+            );
+        }
+    }
+    else {
+        $('#instagram_posts').append('<p>No posts yet!</p>');
+    }
+}
