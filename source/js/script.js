3d88bfca1814a509075ce774cc12eaa74fa19f7a
Jacob Caccamo <jacob.caccamo@gmail.com>
2019-10-04 19:56:34 -0400

    Adding Spacing to Project Page Rows, Finishing Up Animation

--- source/js/script.js
+++ source/js/script.js
@@ -1,108 +1,3 @@
-/*window.onload = function() {
-  Particles.init({
-    selector: '.background',
-    color: "#ffffff",
-    connectParticles: true,
-    maxParticles: 50,
-    speed: .25,
-    responsive: [
-        {
-            breakpoint: 
-                768,
-                options: {
-                    maxParticles: 0,
-                    connectParticles: true
-                }
-        }
-    ]
-  });
-};*/
-$( document ).ready(function() {
-    $('.tabs').tabs();
-
-    // Set the Access Token
-    if ($("#shots").length) {
-        var accessToken = 'b2394526e177d23b8da5a807806fc533a636027de359450b0c9d453b8499d9a1';
-
-        // DRIBBBLE
-        $.ajax({
-            url: 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken,
-            dataType: 'json',
-            type: 'GET',
-            success: function(data) {  
-            if (data.length > 0) { 
-                $.each(data.reverse(), function(i, val) {                
-                $('#shots').prepend(
-                    '<div class="shot col-md-6 col-xl-4"><a class="shot__link" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><img src="'+ val.images.hidpi +'"/></a></div>'
-                    )
-                }); 
-            }
-            else {
-                $('#shots').append('<p>No shots yet!</p>');
-            }
-            }
-        });
-    }
-});
-$(document).ready(function() {
-    if ($('#component--logo__circle').length) {
-        var bar = new ProgressBar.Circle('#component--logo__circle', {
-            strokeWidth: 3,
-            easing: 'easeInOut',
-            duration: 2000,
-            color: '#fff',
-            trailColor: '#000',
-            trailWidth: 0,
-            svgStyle: null
-        });
-
-        bar.animate(1.0); // Number from 0.0 to 1.0
-    }
-});
-$(document).ready(function() {
-
-    if ($('#demo-progress-circle').length) {
-        var bar = new ProgressBar.Circle('#demo-progress-circle', {
-            strokeWidth: 1,
-            easing: 'easeInOut',
-            duration: 2000,
-            color: '#417dc1',
-            trailColor: '#fff',
-            trailWidth: 0,
-            svgStyle: null
-        });
-        bar.animate(1.0); // Number from 0.0 to 1.0
-    }
-
-    if ($('#overall-gpa-circle').length) {
-        var overallGPA = new ProgressBar.Circle('#overall-gpa-circle', {
-            strokeWidth: 1,
-            easing: 'easeInOut',
-            duration: 2000,
-            color: '#417dc1',
-            trailColor: '#fff',
-            trailWidth: 0,
-            svgStyle: null
-        });
-        overallGPA.animate(.99); // Number from 0.0 to 1.0
-    }
-
-    if ($('#major-gpa-circle').length) {
-        var majorGPA = new ProgressBar.Circle('#major-gpa-circle', {
-            strokeWidth: 1,
-            easing: 'easeInOut',
-            duration: 2000,
-            color: '#417dc1',
-            trailColor: '#fff',
-            trailWidth: 0,
-            svgStyle: null
-        });
-        majorGPA.animate(1.0); // Number from 0.0 to 1.0
-    }
-});
-$( document ).ready(function() {
-    $('.tabs').tabs();
-});
 $(document).ready(function() {
     var delay = 200;
     
@@ -167,5 +62,110 @@ $(document).ready(function() {
     }
     function renderScroll() {
         $('.component--scroll').fadeIn();
+    }
+});
+/*window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    color: "#ffffff",
+    connectParticles: true,
+    maxParticles: 50,
+    speed: .25,
+    responsive: [
+        {
+            breakpoint: 
+                768,
+                options: {
+                    maxParticles: 0,
+                    connectParticles: true
+                }
+        }
+    ]
+  });
+};*/
+$( document ).ready(function() {
+    $('.tabs').tabs();
+
+    // Set the Access Token
+    if ($("#shots").length) {
+        var accessToken = 'b2394526e177d23b8da5a807806fc533a636027de359450b0c9d453b8499d9a1';
+
+        // DRIBBBLE
+        $.ajax({
+            url: 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken,
+            dataType: 'json',
+            type: 'GET',
+            success: function(data) {  
+            if (data.length > 0) { 
+                $.each(data.reverse(), function(i, val) {                
+                $('#shots').prepend(
+                    '<div class="shot col-md-6 col-xl-4"><a class="shot__link" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><img src="'+ val.images.hidpi +'"/></a></div>'
+                    )
+                }); 
+            }
+            else {
+                $('#shots').append('<p>No shots yet!</p>');
+            }
+            }
+        });
+    }
+});
+$( document ).ready(function() {
+    $('.tabs').tabs();
+});
+$(document).ready(function() {
+    if ($('#component--logo__circle').length) {
+        var bar = new ProgressBar.Circle('#component--logo__circle', {
+            strokeWidth: 3,
+            easing: 'easeInOut',
+            duration: 2000,
+            color: '#fff',
+            trailColor: '#000',
+            trailWidth: 0,
+            svgStyle: null
+        });
+
+        bar.animate(1.0); // Number from 0.0 to 1.0
+    }
+});
+$(document).ready(function() {
+
+    if ($('#demo-progress-circle').length) {
+        var bar = new ProgressBar.Circle('#demo-progress-circle', {
+            strokeWidth: 1,
+            easing: 'easeInOut',
+            duration: 2000,
+            color: '#417dc1',
+            trailColor: '#fff',
+            trailWidth: 0,
+            svgStyle: null
+        });
+        bar.animate(1.0); // Number from 0.0 to 1.0
+    }
+
+    if ($('#overall-gpa-circle').length) {
+        var overallGPA = new ProgressBar.Circle('#overall-gpa-circle', {
+            strokeWidth: 1,
+            easing: 'easeInOut',
+            duration: 2000,
+            color: '#417dc1',
+            trailColor: '#fff',
+            trailWidth: 0,
+            svgStyle: null
+        });
+        overallGPA.animate(.99); // Number from 0.0 to 1.0
+    }
+
+    if ($('#major-gpa-circle').length) {
+        var majorGPA = new ProgressBar.Circle('#major-gpa-circle', {
+            strokeWidth: 1,
+            easing: 'easeInOut',
+            duration: 2000,
+            color: '#417dc1',
+            trailColor: '#fff',
+            trailWidth: 0,
+            svgStyle: null
+        });
+        majorGPA.animate(1.0); // Number from 0.0 to 1.0
     }
 });
