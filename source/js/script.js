a0ec81ef047972391167ec0940104ce0e1c3703d
Jacob Caccamo <jacob.caccamo@gmail.com>
2019-11-19 22:24:27 -0500

    Starting Work on Marvel Hero

--- source/js/script.js
+++ source/js/script.js
@@ -51,29 +51,32 @@ function renderImages(direction, images) {
 $(document).ready(function() {
     if ($("#shots").length) {
         var accessToken = 'b2394526e177d23b8da5a807806fc533a636027de359450b0c9d453b8499d9a1';
-
+        var url = 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken+'&per_page=100';
         // DRIBBBLE
-        $.ajax({
-            url: 'https://api.dribbble.com/v2/user/access_token='+accessToken,
-            dataType: 'json',
-            type: 'GET',
-            headers: { 'Access-Control-Allow-Origin': 'http://localhost:3000' },
-            success: function(data) {
-                console.log(data);
-                if (data.length > 0) { 
-                    $.each(data.reverse(), function(i, val) {                
-                    $('#shots').prepend(
-                        '<div class="shot col-md-6 col-xl-4"><a class="shot__link" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><img src="'+ val.images.hidpi +'"/></a></div>'
-                        )
-                    }); 
-                }
-                else {
-                    $('#shots').append('<p>No shots yet!</p>');
-                }
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                data = JSON.parse(this.responseText);
+				parseData(data);
             }
-        });
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
     }
 });
+
+function parseData(data) {
+    if (data) {
+        for (var i=0; i < data.length; i++) {
+            if (data[i].projects[0] && data[i].projects[0]['id'] == "951608") {
+                $('#shots').prepend('<div class="shot col-md-6 col-xl-4"><a class="shot__link" target="_blank" href="'+ data[i].html_url +'" title="' + data[i].title + '"><img src="'+ data[i].images.hidpi +'"/></a></div>');
+            }
+        }
+    }
+    else {
+        $('#shots').append('<p>No shots yet!</p>');
+    }
+}
 $(document).ready(function() {
     if ($('#component--logo__circle').length) {
         var bar = new ProgressBar.Circle('#component--logo__circle', {
