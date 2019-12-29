8c19a01471c8d4cd3afa0bfe1910237ed0f71c16
Jacob Caccamo <jacob.caccamo@gmail.com>
2019-12-29 11:52:00 -0800

    Updates to Global Nav

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -49,46 +49,6 @@ $( document ).ready(function() {
     $('.tabs').tabs();
 });
 $(document).ready(function() {
-    if ($("#shots").length) {
-        var projectID = "";
-        // Determine Project
-        if ($('#portfolio--vgs').length) {
-            projectID = "1186682";
-        }
-        else if ($('#portfolio--marvel').length) {
-            projectID = "951608";
-        }
-
-        if (projectID != "") {
-            var accessToken = 'b2394526e177d23b8da5a807806fc533a636027de359450b0c9d453b8499d9a1';
-            var url = 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken+'&per_page=100';
-            // DRIBBBLE
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    data = JSON.parse(this.responseText);
-                    parseData(data, projectID);
-                }
-            };
-            xhttp.open("GET", url, true);
-            xhttp.send();
-        }
-    }
-});
-
-function parseData(data, projectID) {
-    if (data) {
-        for (var i=0; i < data.length; i++) {
-            if (data[i].projects[0] && data[i].projects[0]['id'] == projectID) {
-                $('#shots').prepend('<div class="shot col-md-6 col-xl-4"><a class="shot__link" target="_blank" href="'+ data[i].html_url +'" title="' + data[i].title + '"><img src="'+ data[i].images.hidpi +'"/></a></div>');
-            }
-        }
-    }
-    else {
-        $('#shots').append('<p>No shots yet!</p>');
-    }
-}
-$(document).ready(function() {
     if ($('.component--burger').length) {
         var renderBurgerBool = true;
         var burgerTrigger = $('.section--default--burgerbuilder').offset().top;
@@ -128,6 +88,46 @@ $(document).ready(function() {
         });
     }
 });
+$(document).ready(function() {
+    if ($("#shots").length) {
+        var projectID = "";
+        // Determine Project
+        if ($('#portfolio--vgs').length) {
+            projectID = "1186682";
+        }
+        else if ($('#portfolio--marvel').length) {
+            projectID = "951608";
+        }
+
+        if (projectID != "") {
+            var accessToken = 'b2394526e177d23b8da5a807806fc533a636027de359450b0c9d453b8499d9a1';
+            var url = 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken+'&per_page=100';
+            // DRIBBBLE
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    data = JSON.parse(this.responseText);
+                    parseData(data, projectID);
+                }
+            };
+            xhttp.open("GET", url, true);
+            xhttp.send();
+        }
+    }
+});
+
+function parseData(data, projectID) {
+    if (data) {
+        for (var i=0; i < data.length; i++) {
+            if (data[i].projects[0] && data[i].projects[0]['id'] == projectID) {
+                $('#shots').prepend('<div class="shot col-md-6 col-xl-4"><a class="shot__link" target="_blank" href="'+ data[i].html_url +'" title="' + data[i].title + '"><img src="'+ data[i].images.hidpi +'"/></a></div>');
+            }
+        }
+    }
+    else {
+        $('#shots').append('<p>No shots yet!</p>');
+    }
+}
 $(document).ready(function() {
     if ($('.section--default--cabcs').length) {
         var renderDroneBool = true;
