c57af75dcbec6184f6c8a72bb5e6ef48130d9806
Jacob Caccamo <jacob.caccamo@gmail.com>
2020-07-27 21:07:11 -0400

    Fixing Spacing Issue on Experience

--- source/js/script.js
+++ source/js/script.js
@@ -21,6 +21,11 @@ function renderImages(direction, images) {
         })(i);
     }
 }
+// $(document).ready(function() {
+//     $('.navigation--mobile__menu-button').click(function() {
+//         $('.navigation--mobile__link').toggleClass('open');
+//     });
+// });
 /*window.onload = function() {
   Particles.init({
     selector: '.background',
@@ -40,6 +45,15 @@ function renderImages(direction, images) {
     ]
   });
 };*/
+// $(document).ready(function() {
+//     $('.navigation--primary__menu-button, #menu-close, .navigation--drawer--overlay').click(function() {
+//         $('.navigation--drawer').toggleClass('drawer--open');
+//         $('.navigation--drawer--overlay').toggle();
+//     });
+// });
+$( document ).ready(function() {
+    $('.tabs').tabs();
+});
 $(document).ready(function() {
     if ($('.component--burger').length) {
         var renderBurgerBool = true;
@@ -97,6 +111,22 @@ $(document).ready(function() {
     });
 });
 $(document).ready(function() {
+    if ($('.section--default--cabcs').length) {
+        var renderDroneBool = true;
+        var droneTrigger = $('.section--default--cabcs').offset().top;
+        $(window).scroll(function() {   
+            if ((($(window).scrollTop() + ($(window).height() / 2)) >= droneTrigger) && renderDroneBool) {
+                renderDrone();
+                renderDroneBool = false;
+            }
+        });
+    }
+
+    function renderDrone() {
+        $('.section--default--cabcs').find('.section--default__image').find('img').css({transform: "translate(0, -50%) scale(.8)"});
+    }
+});
+$(document).ready(function() {
     if ($("#shots").length) {
         var projectID = "";
         // Determine Project
@@ -136,22 +166,6 @@ function parseData(data, projectID) {
         $('#shots').append('<p>No shots yet!</p>');
     }
 }
-$(document).ready(function() {
-    if ($('.section--default--cabcs').length) {
-        var renderDroneBool = true;
-        var droneTrigger = $('.section--default--cabcs').offset().top;
-        $(window).scroll(function() {   
-            if ((($(window).scrollTop() + ($(window).height() / 2)) >= droneTrigger) && renderDroneBool) {
-                renderDrone();
-                renderDroneBool = false;
-            }
-        });
-    }
-
-    function renderDrone() {
-        $('.section--default--cabcs').find('.section--default__image').find('img').css({transform: "translate(0, -50%) scale(.8)"});
-    }
-});
 // $(document).ready(function() {
 //     if ($('#component--logo__circle').length) {
 //         var bar = new ProgressBar.Circle('#component--logo__circle', {
@@ -167,20 +181,6 @@ $(document).ready(function() {
 //         bar.animate(1.0); // Number from 0.0 to 1.0
 //     }
 // });
-// $(document).ready(function() {
-//     $('.navigation--primary__menu-button, #menu-close, .navigation--drawer--overlay').click(function() {
-//         $('.navigation--drawer').toggleClass('drawer--open');
-//         $('.navigation--drawer--overlay').toggle();
-//     });
-// });
-// $(document).ready(function() {
-//     $('.navigation--mobile__menu-button').click(function() {
-//         $('.navigation--mobile__link').toggleClass('open');
-//     });
-// });
-$( document ).ready(function() {
-    $('.tabs').tabs();
-});
 $(document).ready(function() {
     var delay = 200;
     
