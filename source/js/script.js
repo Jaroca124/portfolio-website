b73f19d514b3a4f97555b4a628b869bacfacb734
Jacob Caccamo <jacob.caccamo@gmail.com>
2020-09-18 20:07:06 -0400

    Updates to Mobile and Education

--- source/js/script.js
+++ source/js/script.js
@@ -40,20 +40,6 @@ function renderImages(direction, images) {
     ]
   });
 };*/
-// $(document).ready(function() {
-//     $('.navigation--primary__menu-button, #menu-close, .navigation--drawer--overlay').click(function() {
-//         $('.navigation--drawer').toggleClass('drawer--open');
-//         $('.navigation--drawer--overlay').toggle();
-//     });
-// });
-// $(document).ready(function() {
-//     $('.navigation--mobile__menu-button').click(function() {
-//         $('.navigation--mobile__link').toggleClass('open');
-//     });
-// });
-$( document ).ready(function() {
-    $('.tabs').tabs();
-});
 $(document).ready(function() {
     if ($('.component--burger').length) {
         var renderBurgerBool = true;
@@ -181,6 +167,20 @@ $(document).ready(function() {
 //         bar.animate(1.0); // Number from 0.0 to 1.0
 //     }
 // });
+// $(document).ready(function() {
+//     $('.navigation--primary__menu-button, #menu-close, .navigation--drawer--overlay').click(function() {
+//         $('.navigation--drawer').toggleClass('drawer--open');
+//         $('.navigation--drawer--overlay').toggle();
+//     });
+// });
+// $(document).ready(function() {
+//     $('.navigation--mobile__menu-button').click(function() {
+//         $('.navigation--mobile__link').toggleClass('open');
+//     });
+// });
+$( document ).ready(function() {
+    $('.tabs').tabs();
+});
 $(document).ready(function() {
     var delay = 200;
     
